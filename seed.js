c107f546935ab473ef97f453d16956bacaf91892
Chi Doan <chi.doan86@gmail.com>
2022-05-11 22:24:21 +0800

    files

---
+++ seed.js
@@ -0,0 +1,38 @@
+
+
+
+// inquirer --
+
+const { seedDepartment } = require("./department-seed");
+
+// CRUD
+// create
+// read
+// update
+// delete
+
+
+
+
+// view all emp
+
+// create emp
+
+// 
+
+
+async function seed(){
+
+  // seed dept
+  await seedDepartment(10);
+
+  // seed roles
+
+  // seed employee
+
+
+}
+
+
+seed();
+
