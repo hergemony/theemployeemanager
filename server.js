6789b32df857891a6a481b1ae3564c00b8af86a5
Chi Doan <chi.doan86@gmail.com>
2022-05-12 11:57:38 +0800

    sql files

---
+++ server.js
@@ -0,0 +1,6 @@
+// Create main server.js page which will run the backend of the employee manager using mysql
+const inquirer = require("inquirer");
+const fs = require("fs");
+const cTable = require('console.table');
+var mysql = require("mysql");
+const util = require("util");
